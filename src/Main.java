427731979fd5bdd88c08b353cfd935ba5091157c
Ivan <Ivanlesnyak55@gmail.com>
2022-10-28 09:22:33 +0300

    End

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] argv) {
-    
+    System.out.println("\u27a1"+"\u263B"+"\u2763");
+        StringBuffer str = new StringBuffer("HeLLo WOrLd");
+        System.out.println("Початковий рядок:" + str);
+        int ln = str.length();
+        for (int i = 0; i < ln; i++) {
+            Character st = str.charAt(i);
+            str.replace(i, i + 1, Character.toLowerCase(st) + "");
+        }
+        System.out.println("Змінений регістр: " + str.replace(0, 1, "H"));
     }
 }
